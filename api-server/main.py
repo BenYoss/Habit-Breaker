4d14ad0cae31b992e4e3a386d748c874a485efb4
benyoss <benyoss4@gmail.com>
2024-06-17 15:53:54 -0500

    (add) Starting API routes.

--- api-server/main.py
+++ api-server/main.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 def home():
     return "This is the home."
 
+@app.route("/api/persona/create-persona")
+def create_persona():
+    pass
+
+@app.route("/api/persona/update-persona")
+
+def update_persona():
+    pass
+
+@app.route("/api/persona/delete-persona")
+
+def delete_persona():
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
